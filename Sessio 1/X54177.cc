43c1b7c7a2425324df43e8264a5e0b851ff523ab
peremir <57814247+peremir@users.noreply.github.com>
2021-11-15 13:04:11 +0100

    Update X54177.cc

--- Sessio 1/X54177.cc
+++ Sessio 1/X54177.cc
@@ -1,1 +1,23 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    int n, m;
+    cin >> n >> m;
+    
+    for (int i = 1; i <= n; ++i) {
+        
+        int next, sum = 0;
+        while (cin >> next and next != m) {
+            
+            sum += next;
+        }
+        string s;
+        getline(cin,s);
+        cout << "La suma de la secuencia " << i << " es " << sum << endl;
+    }
+}
+
